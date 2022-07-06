387bcd8336eb5f06a5a05e3d824f4bac1aba7d76
Carlos Andres Toro <charlitorodev@gmail.com>
2022-07-06 12:54:01 -0500

    SignIn path ready

--- src/routes/signIn.ts
+++ src/routes/signIn.ts
@@ -1,10 +1,40 @@
 import { Router } from 'express'
+import { models } from '../models'
+import {now} from "mongoose";
+import {signJwt, validatePassword} from '../utils';
 
 const signInRoutes = Router()
 
-signInRoutes.post('/signIn', async (_, res) => {
-    res.send("==> SignIn path")
-    return
+interface UserSignInData {
+    email: string;
+    password: string;
+}
+
+signInRoutes.post('/signIn', async (req, res) => {
+    try {
+        const {email, password}: UserSignInData = req.body
+        const user = await models.User.findOneAndUpdate({email: email}, {
+            last_login: now(),
+            token: signJwt({})
+        }, {new: true})
+        if ( !user ) {
+            res.status(404).json({message: 'Invalid username and/or password'})
+            return
+        }
+        if ( await validatePassword(password, user.password) === false) {
+            res.status(401).json({message: 'Invalid username and/or password'})
+            return
+        }
+        res.status(200).json({
+            id: user._id,
+            creation_date: user.creation_date,
+            update_date: user.update_date,
+            last_login: user.last_login,
+            token: user.token
+        })
+    } catch (e) {
+        res.status(500).json({message: e.message})
+    }
 })
 
 export { signInRoutes }
--- src/utils/cryptPassword.ts
+++ src/utils/cryptPassword.ts
@@ -2,9 +2,19 @@ import { compare, hash } from 'bcrypt'
 
 const saltRounds = 10
 
+/**
+ * Function that encrypt the user password and return a hash
+ * @param password
+ */
 const generatePasswordHash = async ( password ): Promise<string> => {
     return await hash( password, saltRounds )
 }
+
+/**
+ * Function that validate the user password encrypted
+ * @param password
+ * @param cryptPassword
+ */
 const validatePassword = async ( password, cryptPassword ): Promise<boolean> => {
     return await compare( password, cryptPassword )
 }
